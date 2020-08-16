830e435a14e8a0811695788ef02b53c0378802a9
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-16 12:02:28 -0500

    update Bot.py

--- Bot.py
+++ Bot.py
@@ -21,7 +21,7 @@ async def on_ready():
   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="To commands and my coder fire#7010"))
 
 @client.command(usage="Learn how to join a role")
-async def How_do_I_join_Reality(ctx):
+async def How_do_I_join_Xetiq(ctx):
   join=""
   join+="1. What server are you \n"
   join+="-NAE \n"
@@ -31,11 +31,11 @@ async def How_do_I_join_Reality(ctx):
   join+="-OCE \n \n"
   join+="2. What do you want to join as \n \n"
   join+="Cretive Warrior: U need to know to do retakes and edit good. \n \n"
-  join+="Comp Player: U need at least 2k in arena. Play alot of solos/duos/squads \n \n"
+  join+="Comp Player: U need at least 1k in arena. Play alot of solos/duos/squads \n \n"
   join+="TrickShotter: Need to do good trickshots or insane trickshots to join as a Trickshotter \n \n"
   join+="Content Creator: U need atleast 100+ subs to be a content creator \n \n"
   join+="GFX/VFX: U have to make good work and dm staff or owners ur work \n \n"
-  join+="And thats how u Join Reality so try to join this clan and be apart with the members in the clan!"
+  join+="And thats how u Join Xetiq so try to join this clan and be apart with the members in the clan!"
   if(embed_toggle=='0'):
     await ctx.send(join)
   if(embed_toggle=='1'):
@@ -62,7 +62,7 @@ async def help(ctx):
     
 @client.command(hidden=True)
 async def Congratulations(ctx, *, arg=None):
-  if(ctx.message.author.id==699298425124028538):
+  if(ctx.message.author.id==702954010008748174):
     if arg == None:
       if(embed_toggle=='0'):
         await ctx.send("Error: Please specify who you want to congratulate")
