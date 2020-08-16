25cd4fcf1b1a7847b64e8ec1c58a30f1ec284120
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-15 19:39:17 -0500

    Create ping.py

---
+++ cogs/ping.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import time 
+import json
+
+config = json.load(open('config.json'))
+
+class ping(commands.Cog):
+  
+    def __init__(self, client):
+        self.client = client
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Loaded ping.py')
+    
+    @commands.command(usage="Get's the bot's ping")
+    async def ping(self, ctx):
+      before = time.monotonic()
+      message = await ctx.send("Pinging")
+      ping = (time.monotonic() - before) * 1000
+      ping_content = (f":ping_pong:   |   {int(ping)}ms\n"
+             f":timer:   |   {self.client.latency * 1000:.0f}ms")
+      if(config["embed_toggle"]==0):
+        await message.delete()
+        await ctx.send("Ping dose not support non-embeds")
+      if(config["embed_toggle"]==1):
+        ping_embed=discord.Embed(color=0x0000, title="Pong", description=ping_content)
+        await message.edit(embed=ping_embed)
+        
+def setup(client):
+    client.add_cog(ping(client))
