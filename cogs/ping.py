9de4172b6d3c60db419874ffa874db36e61b2c63
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-16 10:42:42 -0500

    update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -19,10 +19,10 @@ class ping(commands.Cog):
       ping = (time.monotonic() - before) * 1000
       ping_content = (f":ping_pong:   |   {int(ping)}ms\n"
              f":timer:   |   {self.client.latency * 1000:.0f}ms")
-      if(embed_toggle==0):
+      if(embed_toggle=='0'):
         await message.delete()
         await ctx.send("Ping dose not support non-embeds")
-      if(embed_toggle==1):
+      if(embed_toggle=='1'):
         ping_embed=discord.Embed(color=0x0000, title="Pong", description=ping_content)
         await message.edit(embed=ping_embed)
         
