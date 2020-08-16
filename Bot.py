ab1d79145c3e0b265012a9d00e08374251678614
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2020-08-15 21:50:00 -0500

    update Bot.py

--- Bot.py
+++ Bot.py
@@ -94,4 +94,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 		
-client.run(config["token"])
+client.run(os. environ['TOKEN'])
